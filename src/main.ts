541f1ad44f8a2f75aaabbcb2fce942b16d98d03f
VATSU_04 <vatsumalambo@gmail.com>
2025-12-14 16:19:36 +0200

    feat: render the text of loading to the screen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
- const app = document.getElementById('app') as HTMLDivElement;
+let load_contain = document.getElementById('chargement') as HTMLParagraphElement;
+const load_words = ['sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds','sdfsfsdfsfds']
+
+for( const w in load_words){
+    load_contain.textContent += ` ${load_words[w]}`
+}
